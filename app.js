e35cfcd24633956a919b4debc97c53ace77a5da7
Aayush Mishra <48150464+mishra-aayush@users.noreply.github.com>
2020-12-21 21:50:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ btn.addEventListener("click", async () => {
 });
 
 const images = ["./images/img1.jpg", "./images/img2.png", "./images/img3.jpg", "./images/img5.jpg", "./images/img6.jpg", "./images/img7.jpg", "./images/img8.jpg",
-"./images/img9.jpg", "./images/img11.jpg", "./images/img12.jpg", "./images/img14.jpg"];
+"./images/img9.jpg", "./images/img11.jpg", "./images/img12.jpg", "./images/img14.jpg", "./images/img15.jpg", "./images/img16.jpg"];
 
 function getRandomImage() {
   return Math.floor(Math.random()*images.length);
