63b7cc7b955f64d9e19e900a9ed7a8f1528b9237
Aayush Mishra <48150464+mishra-aayush@users.noreply.github.com>
2021-12-24 17:30:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ btn.addEventListener("click", async () => {
       console.log(error);
     }
     counter++;
-  }, 500);
+  }, 100);
 });
 
 const images = ["./images/img1.jpg", "./images/img2.png", "./images/img3.jpg", "./images/img5.jpg", "./images/img6.jpg", "./images/img7.jpg", "./images/img8.jpg",
